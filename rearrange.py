57cdb566fff7c866f3a615681e912a2442f11bba
tlambrou <lambrou@gmail.com>
2017-01-26 13:35:03 -0800

    Finished intro script

--- rearrange.py
+++ rearrange.py
@@ -3,10 +3,16 @@ import sys
 
 
 def random_shuffle():
-    rand_index = random.randint(0, len(sys.argv) - 1)
+    rand_index = random.randint(1, len(sys.argv) - 1)
     return sys.argv[rand_index]
 
+# def create_array():
+#     for i in sys.argv:
+
 if __name__ == '__main__':
-    for i in range(len(sys.argv) - 1):
+    for i in range(1, len(sys.argv)):
         random_word = random_shuffle()
-        print random_word + str(" ")
+        print(random_word + " ")
+
+    # for i in sys.argv:
+    #     print(i)
